175ef97ffaa6179eeeeabde440a6bffc3c15acc8
JeremyOlea <43157357+JeremyOlea@users.noreply.github.com>
2020-03-07 17:21:09 -0700

    Update game.js

--- game.js
+++ game.js
@@ -11,6 +11,19 @@ const matrix = [
     [0, 1, 0],
 ];
 
+function arenaSweep() {
+    outer: for(let y = arena.length - 1; y > 0; y--) {
+        for(let x = 0; x < areanpy[y].length; x++) {
+            if(arena[y][x] == 0) {
+                continue outer;
+            }
+        }
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        y++;
+    }
+}
+
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
     for(let y = 0; y < m.length; y++) {
@@ -31,6 +44,52 @@ function createMatrix(w, h) {
     return matrix;
 }
 
+function createPiece(type) {
+    if(type == 'T') {
+        return [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ];
+    } else if(type == 'O') {
+        return [
+            [2, 2],
+            [2, 2],
+        ];
+    } else if(type == 'L') {
+        return [
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
+        ];
+    } else if(type == 'J') {
+        return [
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
+        ];
+    } else if(type == 'I') {
+        return [
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0]
+        ];
+    } else if(type == 'S') {
+        return [
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0],
+        ];
+    } else if(type == 'S') {
+        return [
+            [7, 7, 0],
+            [0, 7, 7],
+            [0, 0, 0],
+        ];
+    }
+}
+
 function draw() {
     context.fillStyle = 'black'; //makes canvas black
     context.fillRect(0,0, canvas.clientWidth, canvas.clientHeight);
@@ -42,7 +101,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if(value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
@@ -71,12 +130,35 @@ function playerDrop() {
     if(collide(arena, player)) {
         player.pos.y--;
         merge(arena, player);
-        player.pos.y = 0;
+        playerReset();
+        arenaSweep();
+    }
+}
+
+function playerReset() {
+    const pieces = 'ILJOTSZ';
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+
+    if(collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
     }
 }
 
 function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
     rotate(player.matrix, dir);
+    while(collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if(offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
 }
 
 function rotate(matrix, dir) {
@@ -100,8 +182,8 @@ function rotate(matrix, dir) {
 
 let dropCounter = 0;
 let dropInterval = 1000;
-
 let lastTime = 0;
+
 function update(time = 0) {
     const deltaTime = time - lastTime;
     lastTime = time;
@@ -114,11 +196,13 @@ function update(time = 0) {
     requestAnimationFrame(update); //repeats
 }
 
+const colors = [null, 'violet', 'orange', 'yellow', 'green', 'blue', 'pink', 'purple'];
+
 const arena = createMatrix(10, 25);
 
 const player = {
-    pos: {x: 5, y: 5},
-    matrix: matrix,
+    pos: {x: 3, y: 0},
+    matrix: createPiece('T'),
 }
 
 document.addEventListener('keydown', event => {
@@ -136,7 +220,7 @@ document.addEventListener('keydown', event => {
     }
 });
 
-
+playerReset();
 update();
 
 
